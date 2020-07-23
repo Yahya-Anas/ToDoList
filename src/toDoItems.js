62b07722574fe48659c11d6ddac60bcefe3f4708
Mark <mark.siebenhaar@edatasystems.de>
2020-07-23 17:01:16 +0200

    test

--- src/toDoItems.js
+++ src/toDoItems.js
@@ -1,56 +1,54 @@
-import React, { Component} from "react";
+import React, {Component} from "react";
 import "./ToDoItems.css";
+
 //import Image from './Foto/generic-male-icon-blue.jpg';
 
 
-
-
-class toDoItems extends Component{
-state= {
-    isCheck: false,
-}
+class toDoItems extends Component {
+    state = {
+        isCheck: false,
+    }
 
     // checkedHandler  =() =>{
     // this.setState({isCheck : !this.state.isCheck})
-    // }
+    // }sadsd
 
-     image = (gender) => {
+    image = (gender) => {
         let imgFile = 'generic-male-icon-blue.jpg';
 
-        if(gender === "Female"){
+        if (gender === "Female") {
             imgFile = 'profile-female-icon.png'
-         }
+        }
 
-        return <img className="imageCard" src={require('./Foto/' + imgFile)} />
+        return <img className="imageCard" src={require('./Foto/' + imgFile)}/>
     }
 
-    createTasks = (item) =>{
-        return<div className="maineCard">
-                <div key={item.key} className="headerItems">
-                        <div className="deleteLogo">
-                            <i onClick={() => this.props.delete(item.key)} id="sendNewSms" className="material-icons">close</i>
-                        </div>
+    createTasks = (item) => {
+        return <div className="maineCard">
+            <div key={item.key} className="headerItems">
+                <div className="deleteLogo">
+                    <i onClick={() => this.props.delete(item.key)} id="sendNewSms" className="material-icons">close</i>
+                </div>
                 {/*<img className="imageCard" src={ require('./Foto/generic-male-icon-blue.jpg') } />*/}
                 {/*    <img className="imageCard" src={this.image()} />*/}
-                        {this.image(item.gender)}
-                        <ol className="card">
-                            <li> Name: {item.text} {item.inputValueLast}</li>
-                            <li> Birth of Date: {item.inputDatum}</li>
-                            <li> Sex: {item.gender}</li>
-                            <li> E-mail: {item.email}</li>
-                            <li> Address: {item.address}</li>
-                        </ol>
-                    </div>
-             </div>
+                {this.image(item.gender)}
+                <ol className="card">
+                    <li> Name: {item.text} {item.inputValueLast}</li>
+                    <li> Birth of Date: {item.inputDatum}</li>
+                    <li> Sex: {item.gender}</li>
+                    <li> E-mail: {item.email}</li>
+                    <li> Address: {item.address}</li>
+                </ol>
+            </div>
+        </div>
 
 
     }
-
 
 
     render() {
         let toDoEntries = this.props.item;
-        return(
+        return (
             <li>
                 {this.createTasks(toDoEntries)}
 
